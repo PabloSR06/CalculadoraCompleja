95c93203843b366823250820d090c33923d494d9
PabloSR06 <47283527+PabloSR06@users.noreply.github.com>
2021-10-19 16:21:03 +0100

    suma resta

--- src/main/java/calculadoraComplejadad/CalculadoraCompleja.java
+++ src/main/java/calculadoraComplejadad/CalculadoraCompleja.java
@@ -3,6 +3,10 @@ package calculadoraComplejadad;
 import comboBox.Alumno;
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -14,15 +18,26 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
 
 public class CalculadoraCompleja extends Application {
 
 	private ComboBox<String> simbolo;
-	private Button igual;
+	private ObjectProperty<String> seleccionado = new SimpleObjectProperty<>();
 
 	private TextField real1;
 	private TextField real2;
 	private TextField realTotal;
+	
+	private IntegerProperty real1Property = new SimpleIntegerProperty();
+	private IntegerProperty imaginario1Property = new SimpleIntegerProperty();
+	
+	private IntegerProperty real2Property = new SimpleIntegerProperty();
+	private IntegerProperty imaginario2Property = new SimpleIntegerProperty();
+	
+	private IntegerProperty realTotalProperty = new SimpleIntegerProperty();
+	private IntegerProperty imaginarioTotalProperty = new SimpleIntegerProperty();
+	
 	private TextField imaginario1;
 	private TextField imaginario2;
 	private TextField imaginarioTotal;
@@ -32,15 +47,16 @@ public class CalculadoraCompleja extends Application {
 	private Label simbolo1;
 	private Label simbolo2;
 	private Label simboloTotal;
+	
+
+	
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
 		simbolo = new ComboBox<String>();
 		simbolo.getItems().addAll("+", "-", "*", "/");
-
-		igual = new Button("=");
-		igual.setOnAction(e -> onIgualButtonAction(e));
+		simbolo.getSelectionModel().selectFirst();
 
 		real1 = new TextField();
 		real1.setPrefWidth(50);
@@ -50,6 +66,7 @@ public class CalculadoraCompleja extends Application {
 
 		realTotal = new TextField();
 		realTotal.setPrefWidth(50);
+		realTotal.setEditable(false);
 
 		imaginario1 = new TextField();
 		imaginario1.setPrefWidth(50);
@@ -59,50 +76,102 @@ public class CalculadoraCompleja extends Application {
 
 		imaginarioTotal = new TextField();
 		imaginarioTotal.setPrefWidth(50);
+		imaginarioTotal.setEditable(false);
 
 		separador = new Separator();
 
 		simbolo1 = new Label("+");
 		simbolo2 = new Label("+");
 		simboloTotal = new Label("+");
+		
+	
 
 		VBox combo = new VBox(5, simbolo);
 		combo.setAlignment(Pos.CENTER);
 
-		HBox suma1 = new HBox(5, real1, simbolo1, imaginario1);
+		HBox suma1 = new HBox(5, real1, simbolo1, imaginario1, new Label("i"));
 		suma1.setAlignment(Pos.CENTER);
 
-		HBox suma2 = new HBox(5, real2, simbolo2, imaginario2);
+		HBox suma2 = new HBox(5, real2, simbolo2, imaginario2, new Label("i"));
 		suma2.setAlignment(Pos.CENTER);
 
-		HBox resultado = new HBox(5, realTotal, simboloTotal, imaginarioTotal);
+		HBox resultado = new HBox(5, realTotal, simboloTotal, imaginarioTotal, new Label("i"));
 		resultado.setAlignment(Pos.CENTER);
 
 		VBox operaciones = new VBox(5, suma1, suma2, separador, resultado);
 		operaciones.setAlignment(Pos.CENTER);
 
-		VBox boton = new VBox(5, igual);
-		boton.setAlignment(Pos.CENTER);
-
-		HBox root = new HBox(5, combo, operaciones, boton);
+		HBox root = new HBox(5, combo, operaciones);
 		root.setAlignment(Pos.CENTER);
 
 		Scene scene = new Scene(root, 320, 200);
-
 		primaryStage.setTitle("Calculadora Compleja");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+		//bindings
+		
+		//real
+		Bindings.bindBidirectional(real1.textProperty(), real1Property,  new NumberStringConverter());
+		Bindings.bindBidirectional(real2.textProperty(), real2Property,  new NumberStringConverter());
+		//imaginario
+		Bindings.bindBidirectional(imaginario1.textProperty(), imaginario1Property,  new NumberStringConverter());
+		Bindings.bindBidirectional(imaginario2.textProperty(), imaginario2Property,  new NumberStringConverter());
+		
+		realTotal.textProperty().bind(realTotalProperty.asString());
+		imaginarioTotal.textProperty().bind(imaginarioTotalProperty.asString());
+		
+		
+		
+		seleccionado.bind(simbolo.getSelectionModel().selectedItemProperty());
+		seleccionado.addListener((o, ov, nv) -> {
+			switch (nv) {
+			case "+":
+				simbolo1.textProperty().setValue("+");
+				simbolo2.textProperty().setValue("+");
+				simboloTotal.textProperty().setValue("+");
+				
+				realTotalProperty.bind(real1Property.add(real2Property));
+				imaginarioTotalProperty.bind(imaginario1Property.add(imaginario2Property));
+				break;
+			case "-":
+				simbolo1.textProperty().setValue("-");
+				simbolo2.textProperty().setValue("-");
+				simboloTotal.textProperty().setValue("-");
+				
+				realTotalProperty.bind(real1Property.subtract(real2Property));
+				imaginarioTotalProperty.bind(imaginario1Property.subtract(imaginario2Property));
+				break;
+			case "*":
+				simbolo1.textProperty().setValue("*");
+				simbolo2.textProperty().setValue("*");
+				simboloTotal.textProperty().setValue("*");
+				
+				System.out.println("hols");
+				break;
+			case "/":
+				simbolo1.textProperty().setValue("/");
+				simbolo2.textProperty().setValue("/");
+				simboloTotal.textProperty().setValue("/");
+				
+				System.out.println("hols");
+				break;
 
-		// real1.textProperty().bindBidirectional
-
-	}
-
-	private void onIgualButtonAction(ActionEvent e) {
-
-		System.out.println(simbolo.getValue());
+			default:
+				break;
+			}
+		});
+		
+		
+		
+/*
+		
+		//System.out.println(simbolo.getValue());
 		switch (simbolo.getValue()) {
 		case "+":
-			real1.textProperty();
+			
+			realTotalProperty.bind(real1Property.add(real2Property));
+			imaginarioTotalProperty.bind(imaginario1Property.add(imaginario2Property));
 			break;
 		case "-":
 			System.out.println("hols");
@@ -117,6 +186,7 @@ public class CalculadoraCompleja extends Application {
 		default:
 			break;
 		}
+*/
 	}
 
 	public static void main(String[] args) {
--- src/main/java/calculadoraComplejadad/Complejo.java
+++ src/main/java/calculadoraComplejadad/Complejo.java
@@ -7,40 +7,26 @@ public class Complejo {
 	
 	private DoubleProperty real = new SimpleDoubleProperty();
 	private DoubleProperty imaginario = new SimpleDoubleProperty();
-	
-
-	
-
 
 	public final DoubleProperty realProperty() {
 		return this.real;
 	}
 	
-
-
 	public final double getReal() {
 		return this.realProperty().get();
 	}
 	
-
-
 	public final void setReal(final double real) {
 		this.realProperty().set(real);
 	}
 	
-
-
 	public final DoubleProperty imaginarioProperty() {
 		return this.imaginario;
 	}
 	
-
-
 	public final double getImaginario() {
 		return this.imaginarioProperty().get();
 	}
-	
-
 
 	public final void setImaginario(final double imaginario) {
 		this.imaginarioProperty().set(imaginario);
